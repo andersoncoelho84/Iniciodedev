5485d3a223c807c5c72cdffdb3554a54927f4e7a
Anderson da Conceição Valença Coelho <ART0067849@aluno.firjansenaisesi.com.br>
2025-10-02 21:21:10 -0300

    atualizacao

--- integracao_banco_orm.py
+++ integracao_banco_orm.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String 
-from sqlalchemy .orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, CheckConstraint 
+from sqlalchemy .orm import declarative_base, sessionmaker, relationship 
 
 # method factory(fabrica de classes)
 Base = declarative_base()
@@ -13,9 +13,30 @@ class Aluno(Base):
     nome = Column(String(56), nullable=False)
     idade = Column(Integer, nullable=False)
     email = Column(String, unique=True, nullable=False)
+
+class Disciplina(Base):
+    __tablbename__ ='disciplina'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(62), nullable=False, unique=True)
+    carga_horaria = Column(Integer, nullable=False)
+    CheckConstraint('carga_horarioa > 0')
+
+    #
+    #
+    alunos = relationship('AlunoDisciplina', back_populates='Disciplina')
+
+    def __repe__(self):
+        return f"<Disciplina(id=({self.id}), noeme=({self.nome}), carga_horaria={self.carga_horaria})>"
+
+
+#back_populates -> referencia reciproca
+#em outras palafras -> quando você associa a Aluno à AlunoDisciplina é associada a Aluno
+    disciplina = relationship('AlunoDisciplina', back_populates='Aluno')
+     
     #metodo magico
     def __repr__(self):
-        return f"Aluno(id={self.id}, nome='{self.nome}',idade={self.idade},email='{self.email}')>"
+        return f"<Aluno(id={self.id}, nome='{self.nome}',idade={self.idade},email='{self.email}')>"
 
 #Criar engine(cria conexão com o bd)
 engine = create_engine("sqlite:///teste_com_orm2.br", echo=True, future=True)
@@ -43,6 +64,7 @@ with Session() as session:
 
     for aluno in resultado:
         print(aluno.id, aluno.nome, aluno.idade, aluno.email)
+
 
 
 
